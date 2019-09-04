f9a5bfddcadf8752406cc474b1dbe3acd6c0b036
Arian <arian.kulmer@web.de>
2019-09-04 19:40:33 +0200

    msm8953-common: DNM: Add minimal sepolicy to compile
    Change-Id: Id03d4df67a4669e5d2535ef608e45249292c94a5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,6 +192,7 @@ endif
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy-minimal
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
